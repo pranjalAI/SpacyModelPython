a259e1c8bc1b58918385277095d20a672593ff13
Pranjal Saxena <pranjal.saxena2012@gmail.com>
2024-01-06 13:19:10 +0530

    download_model.py

---
+++ dowload_model.py
@@ -0,0 +1,8 @@
+import spacy
+
+def download_model(model_name):
+    spacy.cli.download(model_name)
+
+if __name__ == "__main__":
+    model_name = "en_core_web_md"  # or any other model you need
+    download_model(model_name)
