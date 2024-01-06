0197fd4cbd398dc752609786babe94a560ad17d8
Pranjal Saxena <pranjal.saxena2012@gmail.com>
2024-01-06 09:33:39 +0000

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -4,5 +4,5 @@ def download_model(model_name):
     spacy.cli.download(model_name)
 
 if __name__ == "__main__":
-    model_name = "en_core_web_lg"  # or any other model you need
+    model_name = "en_core_web_trf"  # or any other model you need
     download_model(model_name)
